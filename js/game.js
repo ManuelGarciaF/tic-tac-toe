b4e28557893a5e56a343bab4d07563fa9e76b0f0
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-16 17:23:27 -0300

    add event listeners to board tiles

--- js/game.js
+++ js/game.js
@@ -1,63 +1,69 @@
 // Tic Tac Toe project by Manuel Garcia Frigo
 
-const gameBoard = (() => {
+const game = (() => {
   "use strict";
 
-  // ⭕ ❌
+  let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-  let board = [
-    "", "", "",
-    "", "", "",
-    "", "", "",
-  ]
+  let _currentPlayer = 1;
 
-  const addMark = (mark, index) => {
-    if (board[index] !== "") return false;
-    board[index] = mark;
+  const _checkBoard = () => {};
+
+  const addMark = index => {
+    if (board[index] !== 0) return false;
+    board[index] = _currentPlayer;
+    _currentPlayer = _currentPlayer === 1 ? 2 : 1; // Toggle _currentPlayer
     return true;
-  }
+  };
 
-  return { board, addMark };
+  const getCurrentPlayer = () => _currentPlayer;
+
+  return {
+    board,
+    getCurrentPlayer,
+    addMark
+  };
 })();
 
 const displayController = (() => {
   "use strict";
 
-  let _tiles = [];
+  const _refresh = () => {
+    game.board.forEach((elem, index) => {
+      if (elem !== 0)
+        document
+          .querySelector(`[data-index="${index}"]`)
+          .setAttribute("data-player", elem);
+    });
+  };
 
   const populate = () => {
-    const container = document.querySelector(".game-board")
-  
-    for (let i = 0; i < 9; i++) {
-      const button = document.createElement("button")
-      button.classList.add("board-locker")
-      _tiles.push(button)
-      container.appendChild(button)
-    }
-  }
+    const container = document.querySelector(".game-board");
 
-  const refresh = (board) => {
-    _tiles.forEach((elem, index) => {
-      if (board[index] !== "")
-      elem.setAttribute("data-player", board[index])
-    })
-  }
+    for (let i = 0; i < 9; i++) {
+      const button = document.createElement("button");
+      button.classList.add("board-locker");
+      button.setAttribute("data-index", i);
+      container.appendChild(button);
+
+      button.addEventListener("click", event => {
+        game.addMark(event.target.getAttribute("data-index"));
+        _refresh();
+
+        document.querySelector(
+          "#player-num"
+        ).textContent = game.getCurrentPlayer();
+      });
+    }
+
+    document.querySelector("#player-num").textContent = game.getCurrentPlayer();
+  };
 
   return {
-    populate,
-    refresh
-  }
-
+    populate
+  };
 })();
 
-const PlayerFactory = (name, number) => {
-  return {
-    name,
-    number
-  }
-}
 
-const player1 = PlayerFactory("◯", 1)
-const player2 = PlayerFactory("❌", 2)
 
-displayController.populate()
+displayController.populate();
